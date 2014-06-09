5062cda70498eb50a62649aa1c2ef48304369d60
Сергей <vost_sergey@mail.ru>
2014-06-09 19:53:03 +0400

    Добавил ндостающие коды ошибок

--- workspace/ShopClient/src/stx/shopclient/webservice/ServiceResponseCode.java
+++ workspace/ShopClient/src/stx/shopclient/webservice/ServiceResponseCode.java
@@ -6,9 +6,11 @@ public class ServiceResponseCode
 	  public static final int OK = 2; //Операция выполнена
 	  public static final int EXISTS_USER = 3; //Пользователь с таким именем уже есть в системе
 	  public static final int NO_VALIDE_ARGS = 5; //Параметры не прошли валидацию
-	  
+	  public static final int ITEM_NOT_FOUND = 6; //Элемент с таким идентификатором не найден
+	  public static final int ORDER_CANT_DELETE = 7; //Для элемента сформирован счет, удаление невозможно
 	  
 	  public static final int SQL_SERVER_ERROR = 1000; //Внутрянняя ошибка SQL сервера
+	  public static final int RETURN_EMPTY_STRING = 1001; //Возникает если процедура вернула пустую строку. По Вашему запросу ничего не найдено.
 	  public static final int WEB_SERVER_ERROR = 1002; //ошибка http code 500, внутренняя ошибка сервера
 	  public static final int ACCESS_DENIED = 1003; //Доступ по данному ключу невозможен
 }
