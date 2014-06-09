18a49a02fe3df00b723d2a3f64f249de56543b9a
Сергей <vost_sergey@mail.ru>
2014-06-09 16:22:59 +0400

    Token

--- workspace/ShopClient/src/stx/shopclient/entity/Token.java
+++ workspace/ShopClient/src/stx/shopclient/entity/Token.java
@@ -1,8 +1,22 @@
 package stx.shopclient.entity;
 
+import java.util.Date;
+
 public class Token
 {
+	private String _code;
 	private String _token;
+	private Date _begDate;
+	private int _interval;
+	
+	public String getCode()
+	{
+		return _code;
+	}
+	public void setCode(String code)
+	{
+		_code= code;
+	}
 	
 	public String getToken()
 	{
@@ -11,5 +25,23 @@ public class Token
 	public void setToken(String token)
 	{
 		_token= token;
+	}
+	
+	public Date getBegDate()
+	{
+		return _begDate;
+	}
+	public void setBegDate(Date begDate)
+	{
+		_begDate = begDate;
+	}
+	
+	public int getInterval()
+	{
+		return _interval;
+	}
+	public void setInterval(int interval)
+	{
+		_interval = interval;
 	}
 }
--- workspace/ShopClient/src/stx/shopclient/parsers/TokenParser.java
+++ workspace/ShopClient/src/stx/shopclient/parsers/TokenParser.java
@@ -7,13 +7,19 @@ import stx.shopclient.entity.Token;
 public class TokenParser extends BaseParser<Token>
 {
 	private final String ITEM_NAME = "Result";
+	private final String CODE_NAME = "Code";
 	private final String TOKEN_NAME = "token";
+	private final String BEG_DATE_NAME = "BegDate";
+	private final String INTERVAL_NAME = "Interval";
 	
 	@Override
 	public Token getElement(Element e)
 	{
 		Token token = new Token();
+		token.setCode(super.getValue(e, CODE_NAME));
 		token.setToken(super.getValue(e, TOKEN_NAME));
+		token.setBegDate(super.getValueDate(e, BEG_DATE_NAME));
+		token.setInterval(super.getValueInt(e, INTERVAL_NAME));
 		return token;
 	}
 
