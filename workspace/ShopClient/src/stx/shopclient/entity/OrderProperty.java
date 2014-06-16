00e3b178a6ded167f4be79c45c2d4e65507fd9e2
alexandernedelko <alexander.nedelko@outlook.com>
2014-06-16 19:16:49 +0400

    q

--- workspace/ShopClient/src/stx/shopclient/entity/OrderProperty.java
+++ workspace/ShopClient/src/stx/shopclient/entity/OrderProperty.java
@@ -2,12 +2,17 @@ package stx.shopclient.entity;
 
 public class OrderProperty extends KeyValue<String>
 {
+	public static final String COUNT_PROPERTY_NAME = "Count";
+
 	private long _orderId;
-	
-	public long getOrderId() {
+
+	public long getOrderId()
+	{
 		return _orderId;
 	}
-	public void setOrderId(long orderId) {
+
+	public void setOrderId(long orderId)
+	{
 		_orderId = orderId;
 	}
 }
--- workspace/ShopClient/src/stx/shopclient/orderactivity/OrderActivity.java
+++ workspace/ShopClient/src/stx/shopclient/orderactivity/OrderActivity.java
@@ -3,9 +3,11 @@ package stx.shopclient.orderactivity;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
+import android.os.AsyncTask;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -20,11 +22,13 @@ import stx.shopclient.entity.CatalogItem;
 import stx.shopclient.entity.CatalogSettings;
 import stx.shopclient.entity.KeyValue;
 import stx.shopclient.entity.OrderProperty;
+import stx.shopclient.entity.Token;
 import stx.shopclient.entity.properties.PropertyDescriptor;
 import stx.shopclient.itemactivity.ItemActivity;
 import stx.shopclient.repository.Repository;
 import stx.shopclient.styles.ColorButtonDrawable;
 import stx.shopclient.ui.common.properties.PropertiesList;
+import stx.shopclient.webservice.WebClient;
 
 public class OrderActivity extends BaseActivity implements OnClickListener
 {
@@ -44,16 +48,18 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 
 		Intent intent = getIntent();
 		ItemId = intent.getLongExtra(ItemActivity.ITEM_ID_EXTRA_KEY, 0);
-		
-		CatalogSettings settings = Repository.get(this).getCatalogManager().getSettings();
-		CatalogItem item = Repository.get(this).getItemsManager().getItem(ItemId);
-		
+
+		CatalogSettings settings = Repository.get(this).getCatalogManager()
+				.getSettings();
+		CatalogItem item = Repository.get(this).getItemsManager()
+				.getItem(ItemId);
+
 		lblItemName = (TextView) view.findViewById(R.id.lblItemName);
 		plProperies = (PropertiesList) view.findViewById(R.id.plProperties);
 		llCountable = (LinearLayout) view.findViewById(R.id.llCountable);
-		btnOrder = (Button)view.findViewById(R.id.btnOrder);
-		imgIco = (ImageView)view.findViewById(R.id.imgIco);
-		
+		btnOrder = (Button) view.findViewById(R.id.btnOrder);
+		imgIco = (ImageView) view.findViewById(R.id.imgIco);
+
 		llCountable.setVisibility(View.GONE);
 
 		lblItemName.setText(item.getName());
@@ -61,35 +67,24 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 		btnOrder.setOnClickListener(this);
 		btnOrder.setBackground(getBueButtonDrawable(settings));
 		btnOrder.setTextColor(settings.getForegroundColor());
-		
+
 		plProperies.setAllowClear(false);
-		
-		imgIco.setImageBitmap(Repository.get(this).getImagesManager().getImage(item.getIco()));
-		
+
+		imgIco.setImageBitmap(Repository.get(this).getImagesManager()
+				.getImage(item.getIco()));
+
 		properties.addAll(item.getOrderProperties());
 		plProperies.setProperties(properties);
-		
+
 		return view;
 	}
 
 	@Override
 	public void onClick(View v)
 	{
-		ArrayList<OrderProperty> items = new ArrayList<OrderProperty>();
-		for(PropertyDescriptor el : properties)
-		{
-			OrderProperty item = new OrderProperty();
-			item.setName(el.getName());
-			item.setValue(el.getStringValue());
-			items.add(item);
-		}
-		
-		Repository.get(this).getOrderManager().addOrderItem(ItemId, items);
-		Toast.makeText(this, "Товар добавлен в карзину",Toast.LENGTH_SHORT).show();
-		finish();
+		new AddOrderTask().execute();
 	}
-	
-	
+
 	private Drawable getBueButtonDrawable(CatalogSettings settings)
 	{
 		StateListDrawable drawable = new StateListDrawable();
@@ -97,9 +92,71 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 		Drawable press = new ColorButtonDrawable(settings.getPressedColor());
 		Drawable disable = new ColorButtonDrawable(settings.getDisableColor());
 
-		drawable.addState(new int[] { android.R.attr.state_pressed }, press);
-		drawable.addState(new int[] { -android.R.attr.state_enabled }, disable);
+		drawable.addState(new int[]
+		{ android.R.attr.state_pressed }, press);
+		drawable.addState(new int[]
+		{ -android.R.attr.state_enabled }, disable);
 		drawable.addState(new int[0], normal);
 		return drawable;
+	}
+
+	class AddOrderTask extends AsyncTask<Void, Void, Void>
+	{
+		ProgressDialog dialog;
+		Throwable exception;
+
+		@Override
+		protected void onPreExecute()
+		{
+			dialog = ProgressDialog.show(OrderActivity.this, "Загрузка",
+					"Добавление товара в корзину");
+		}
+
+		@Override
+		protected Void doInBackground(Void... params)
+		{
+			try
+			{
+				ArrayList<OrderProperty> items = new ArrayList<OrderProperty>();
+				double count = 0;
+				for (PropertyDescriptor el : properties)
+				{
+					OrderProperty item = new OrderProperty();
+					item.setName(el.getName());
+					item.setValue(el.getStringValue());
+					items.add(item);
+
+					if (item.getName()
+							.equals(OrderProperty.COUNT_PROPERTY_NAME))
+						count = Double.parseDouble(item.getValue());
+				}
+
+				WebClient client = createWebClient();
+				client.addOrder(Token.getCurrent(), ItemId, count, items);
+			}
+			catch (Throwable ex)
+			{
+				exception = ex;
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result)
+		{
+			dialog.dismiss();
+
+			if (exception != null)
+				Toast.makeText(OrderActivity.this,
+						exception.getLocalizedMessage(), Toast.LENGTH_LONG)
+						.show();
+			else
+			{
+				Toast.makeText(OrderActivity.this, "Товар добавлен в карзину",
+						Toast.LENGTH_LONG).show();
+				finish();
+			}
+		}
 	}
 }
