5f220e4be7fb13c27fa77e45aa19b647396950ac
alexandernedelko <alexander.nedelko@outlook.com>
2014-08-21 19:28:01 +0400

    1

--- workspace/ShopClient/src/stx/shopclient/historyactivity/PaymentDescriptionFragment.java
+++ workspace/ShopClient/src/stx/shopclient/historyactivity/PaymentDescriptionFragment.java
@@ -2,6 +2,7 @@ package stx.shopclient.historyactivity;
 
 import stx.shopclient.R;
 import stx.shopclient.cartactivity.CartActivity;
+import stx.shopclient.orderactivity.OrderActivity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
@@ -20,14 +21,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class PaymentDescriptionFragment extends DialogFragment implements android.view.View.OnClickListener
+public class PaymentDescriptionFragment extends DialogFragment implements
+		android.view.View.OnClickListener
 {
-	private static final String ANANIM_NAME="IsAnanim"; 
+	private static final String ANANIM_NAME = "IsAnanim";
 	EditText _txtMain;
 	EditText _txtPhone;
 	TextView _lblPhone;
-	
-	
+
 	public static PaymentDescriptionFragment get(boolean isAnanim)
 	{
 		PaymentDescriptionFragment value = new PaymentDescriptionFragment();
@@ -36,46 +37,52 @@ public class PaymentDescriptionFragment extends DialogFragment implements androi
 		value.setArguments(bundle);
 		return value;
 	}
-	
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
-	{		
-		View v = inflater.inflate(stx.shopclient.R.layout.pyment_description_fragment, container, false);
-		_txtMain = (EditText)v.findViewById(stx.shopclient.R.id.txtComment);
-		_txtPhone = (EditText)v.findViewById(stx.shopclient.R.id.txtPhone);
-		_lblPhone = (TextView)v.findViewById(R.id.lblPhone);
-		
-		_txtPhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
-		
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		View v = inflater.inflate(
+				stx.shopclient.R.layout.pyment_description_fragment, container,
+				false);
+		_txtMain = (EditText) v.findViewById(stx.shopclient.R.id.txtComment);
+		_txtPhone = (EditText) v.findViewById(stx.shopclient.R.id.txtPhone);
+		_lblPhone = (TextView) v.findViewById(R.id.lblPhone);
+
+		_txtPhone
+				.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
 		if (!getArguments().getBoolean(ANANIM_NAME, true))
 		{
 			_txtPhone.setVisibility(View.GONE);
 			_txtPhone = null;
 			_lblPhone.setVisibility(View.GONE);
 		}
-		
-		((Button)v.findViewById(R.id.btnOk)).setOnClickListener(this);
-		((Button)v.findViewById(R.id.btnCancel)).setOnClickListener(this);
-		
+
+		((Button) v.findViewById(R.id.btnOk)).setOnClickListener(this);
+		((Button) v.findViewById(R.id.btnCancel)).setOnClickListener(this);
+
 		return v;
 	}
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState)
 	{
 		Dialog dlg = super.onCreateDialog(savedInstanceState);
 		dlg.setCanceledOnTouchOutside(false);
 		dlg.setTitle(R.string.comment);
-		this.setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Holo_Light_Dialog);
-		
-		//final Drawable d = new ColorDrawable(Color.BLACK);
-        //d.setAlpha(0);
-        //dlg.getWindow().setBackgroundDrawable(d);
-		
+		this.setStyle(DialogFragment.STYLE_NORMAL,
+				android.R.style.Theme_Holo_Light_Dialog);
+
+		// final Drawable d = new ColorDrawable(Color.BLACK);
+		// d.setAlpha(0);
+		// dlg.getWindow().setBackgroundDrawable(d);
+
 		return dlg;
 	}
-	
+
 	@Override
 	public void onClick(View v)
-	{		
+	{
 		if (v.getId() == R.id.btnOk)
 		{
 			String comment = "";
@@ -83,18 +90,23 @@ public class PaymentDescriptionFragment extends DialogFragment implements androi
 			{
 				if (_txtPhone.getText().length() < 15)
 				{
-				Toast.makeText(getActivity(), R.string.phone_error_message, Toast.LENGTH_SHORT).show();
-				return;
+					Toast.makeText(getActivity(), R.string.phone_error_message,
+							Toast.LENGTH_SHORT).show();
+					return;
 				}
 				else
 				{
-					comment = "Телефон: "+_txtPhone.getText().toString()+"\n";
+					comment = "Телефон: " + _txtPhone.getText().toString()
+							+ "\n";
 				}
 			}
-			comment += _txtMain.getText().toString();  
-			((CartActivity)getActivity()).SetComment(comment);
+			comment += _txtMain.getText().toString();
+			if (getActivity() instanceof CartActivity)
+				((CartActivity) getActivity()).SetComment(comment);
+			else if(getActivity() instanceof OrderActivity)
+				((OrderActivity) getActivity()).SetComment(comment);
 		}
 		dismiss();
 	}
-	
+
 }
--- workspace/ShopClient/src/stx/shopclient/orderactivity/OrderActivity.java
+++ workspace/ShopClient/src/stx/shopclient/orderactivity/OrderActivity.java
@@ -4,21 +4,28 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import android.app.DialogFragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
 import android.os.AsyncTask;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import stx.shopclient.BaseActivity;
 import stx.shopclient.R;
+import stx.shopclient.cartactivity.CartActivity;
+import stx.shopclient.cartactivity.CrosssaleActivity;
+import stx.shopclient.entity.Catalog;
 import stx.shopclient.entity.CatalogItem;
 import stx.shopclient.entity.CatalogSettings;
 import stx.shopclient.entity.KeyValue;
@@ -26,6 +33,8 @@ import stx.shopclient.entity.OrderProperty;
 import stx.shopclient.entity.Token;
 import stx.shopclient.entity.properties.NumberPropertyDescriptor;
 import stx.shopclient.entity.properties.PropertyDescriptor;
+import stx.shopclient.historyactivity.PaymentDescriptionFragment;
+import stx.shopclient.historyactivity.PaymentListActivity;
 import stx.shopclient.itemactivity.ItemActivity;
 import stx.shopclient.mainmenu.MainMenuItem;
 import stx.shopclient.repository.OrdersManager;
@@ -43,6 +52,7 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 	private PropertiesList plProperies;
 	private LinearLayout llCountable;
 	private Button btnOrder;
+	private Button btnAddPayment;
 	List<PropertyDescriptor> properties = new ArrayList<PropertyDescriptor>();
 	CatalogItem _item;
 
@@ -63,6 +73,7 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 		plProperies = (PropertiesList) view.findViewById(R.id.plProperties);
 		llCountable = (LinearLayout) view.findViewById(R.id.llCountable);
 		btnOrder = (Button) view.findViewById(R.id.btnOrder);
+		btnAddPayment = (Button) view.findViewById(R.id.btnAddPayment);
 		imgIco = (ImageView) view.findViewById(R.id.imgIco);
 
 		llCountable.setVisibility(View.GONE);
@@ -72,6 +83,10 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 		btnOrder.setOnClickListener(this);
 		btnOrder.setBackground(getButtonDrawable(settings));
 		btnOrder.setTextColor(settings.getForegroundColor());
+
+		btnAddPayment.setOnClickListener(this);
+		btnAddPayment.setBackground(getButtonDrawable(settings));
+		btnAddPayment.setTextColor(settings.getForegroundColor());
 
 		plProperies.setAllowClear(false);
 
@@ -93,6 +108,19 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 
 		plProperies.setProperties(properties);
 
+		Display display = getWindowManager().getDefaultDisplay();
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		display.getMetrics(displayMetrics);
+
+		GridLayout.LayoutParams params = (GridLayout.LayoutParams) btnOrder
+				.getLayoutParams();
+		params.width = (int) (displayMetrics.widthPixels / 2 - 10);
+		btnOrder.setLayoutParams(params);
+
+		params = (GridLayout.LayoutParams) btnAddPayment.getLayoutParams();
+		params.width = (int) (displayMetrics.widthPixels / 2 - 10);
+		btnAddPayment.setLayoutParams(params);
+
 		return view;
 	}
 
@@ -105,8 +133,7 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 			return super.initMainMenuItem(item);
 	}
 
-	@Override
-	public void onClick(View v)
+	boolean validateProperties()
 	{
 		for (PropertyDescriptor el : properties)
 		{
@@ -114,11 +141,37 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 			{
 				Toast.makeText(this, "Нужно заполнить параметры заказа",
 						Toast.LENGTH_LONG).show();
-				return;
+				return false;
 			}
 		}
 
-		new AddOrderTask().execute();
+		return true;
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		if (v.getId() == R.id.btnOrder)
+		{
+			if (validateProperties())
+				new AddOrderTask().execute();
+		}
+		else if (v.getId() == R.id.btnAddPayment)
+		{
+			if (validateProperties())
+			{
+				DialogFragment wd = PaymentDescriptionFragment.get(!Token
+						.getCurrent().getAuthorize());
+				wd.show(getFragmentManager(), "Comment");
+			}
+		}
+	}
+	
+	public void SetComment(String comment)
+	{
+		PaymentTask task = new PaymentTask();
+		task.comment = comment;
+		task.execute();
 	}
 
 	class AddOrderTask extends AsyncTask<Void, Void, Void>
@@ -157,7 +210,7 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 		{
 			if (isDestroyed())
 				return;
-			
+
 			dialog.dismiss();
 
 			if (exception != null)
@@ -169,6 +222,85 @@ public class OrderActivity extends BaseActivity implements OnClickListener
 				Toast.makeText(OrderActivity.this, "Товар добавлен в корзину",
 						Toast.LENGTH_LONG).show();
 				finish();
+			}
+		}
+	}
+	
+	class PaymentTask extends AsyncTask<Void, Void, Void>
+	{
+		Throwable exception;
+		ProgressDialog dialog;
+
+		public String comment;
+
+		Collection<CatalogItem> crosssaleItems;
+
+		@Override
+		protected void onPreExecute()
+		{
+			dialog = ProgressDialog.show(OrderActivity.this, "Загрузка",
+					"Оформирование заказа");
+		}
+
+		@Override
+		protected Void doInBackground(Void... params)
+		{
+			try
+			{
+				WebClient client = createWebClient();
+				
+				Collection<OrderProperty> orderProps = OrdersManager
+						.getOrderPropertiesFromDescriptors(properties);
+				
+				client.addOrder(Token.getCurrent(), ItemId, orderProps);
+				
+				long paymentNumber = client.addPayment(Token.getCurrent(),
+						Repository.get(null).CatalogId, comment);
+				crosssaleItems = client.getCrosssale(Token.getCurrent(),
+						paymentNumber);
+			}
+			catch (Throwable ex)
+			{
+				exception = ex;
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result)
+		{
+			if (isDestroyed())
+				return;
+
+			dialog.dismiss();
+
+			if (exception != null)
+				Toast.makeText(OrderActivity.this,
+						exception.getLocalizedMessage(), Toast.LENGTH_LONG)
+						.show();
+			else
+			{
+				Repository.get(null).getOrderManager()
+						.setOrderCatalogs(new ArrayList<Catalog>());
+				Toast.makeText(OrderActivity.this, "Заказ оформлен",
+						Toast.LENGTH_LONG).show();
+
+				finish();
+
+				if (crosssaleItems != null && crosssaleItems.size() > 0)
+				{
+					CrosssaleActivity.crosssaleItems = crosssaleItems;
+					Intent intent = new Intent(OrderActivity.this,
+							CrosssaleActivity.class);
+					startActivity(intent);
+				}
+				else
+				{
+					Intent intent = new Intent(OrderActivity.this,
+							PaymentListActivity.class);
+					startActivity(intent);
+				}
 			}
 		}
 	}
